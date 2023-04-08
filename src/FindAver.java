16502e2dad06d65a0155b505684e2f79383ffd09
Miras <mirasikk1609@gmail.com>
2023-04-08 12:05:00 +0600

    finding average value from array

---
+++ src/FindAver.java
@@ -0,0 +1,27 @@
+package src;
+import java.util.Scanner;
+
+public class FindAver {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for(int i=0; i<num; i++ ){
+            arr[i] = sc.nextInt();
+        }
+        int minVal = findaver(arr);
+        System.out.println(minVal);
+    }
+    private static int findaver(int[] arr) {
+        int ov;
+        int s = 0;
+        if (arr.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty.");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            s = s + arr[i];
+        }
+        ov = s/arr.length;
+        return ov;
+    }
+}
